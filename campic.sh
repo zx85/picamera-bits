e5422161f6fcd64f96f6f3947008bba2b4954503
james@pinfinity <github@mus-ic.co.uk>
2022-09-03 00:37:01 +0100

    fixing the time check

--- campic.sh
+++ campic.sh
@@ -1,24 +1,16 @@
-#!/usr/bin/bash
-suntimedir="/home/pi/picamera-bits/tmp"
+#!/usr/bin/env bash
+suntimedir="/home/${USER}/picamera-bits/tmp"
 
 daytime () {
-  log "checking daytime"
   isdaytime=0
   if [ -d ${suntimedir} ] ; then
     if [ -f ${suntimedir}/sunrise.txt ] && [ -f ${suntimedir}/sunset.txt ] ; then
-      thistime=$(date +%s) 
+      suntime=$(date +%s)
       sunrise=$(cat ${suntimedir}/sunrise.txt)
       sunset=$(cat ${suntimedir}/sunset.txt)
-      log "thistime is ${thistime}"
-      log "sunrise is ${sunrise}"
-      log "sunset is ${sunset}"
-#      if [ ${thistime} -gt ${sunrise} ] ; then 
-#        log "it is after sunrise"
-#        if [ ${thistime} -lt ${sunset} ] ; then
-#          log "it is before sunset"
-#          isdaytime=0
-#        fi
-#      fi
+      if [ ${suntime} -gt ${sunrise} ] && [ ${suntime} -lt ${sunset} ] ; then
+          isdaytime=1
+      fi
     else
       log "Missing sunrise.txt or sunset.txt in ${suntimedir} - please run suntime.sh"
       exit 1
@@ -39,7 +31,8 @@ log () {
 srcdir="/media/cam"
 filename="${srcdir}/$(date +%Y-%m-%d-%H%M%S).jpg"
 
-if daytime ; then
+daytime
+if [ $? -eq 1 ] ; then
   log "Daytime - taking the picture ${filename}"
 #  libcamera-jpeg --height=972 --width=1296 -o ${filename}
 else
